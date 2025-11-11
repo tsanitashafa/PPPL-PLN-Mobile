7843d10904025b7c655254602687cdafab3f50da
Final Project ASD <manzil260405@gmail.com>
2025-11-11 12:39:27 +0700

    feat: 5026231037-add views edit-profil-2.blade, edit-profile-1, homepage, pembayaran, transaksi gagal, transaksi berhasil

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('homepage', function () {
+    return view('homepage');
+});
+
+Route::get('edit-profile-1', function () {
+    return view('edit-profile-1');
+});
+
+Route::get('edit-profil-2', function () {
+    return view('edit-profil-2');
+});
+
+Route::get('transaksi-berhasil', function () {
+    return view('transaksi-berhasil');
+});
+
+Route::get('pembayaran', function () {
+    return view('pembayaran');
+
+});
+
+
+Route::get('transaksi-gagal', function () {
+    return view('transaksi-gagal');
+});
+
