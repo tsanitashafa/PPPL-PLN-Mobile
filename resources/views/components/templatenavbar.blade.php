9bcf4226072c830860d47e3f6fad0f402d07af9a
tsanitashafa <tsanitashafa@gmail.com>
2025-12-02 09:10:19 +0700

    revisi

--- resources/views/components/templatenavbar.blade.php
+++ resources/views/components/templatenavbar.blade.php
@@ -2,17 +2,17 @@
 
 @props(['title', 'backUrl'])
 
-<div class="d-flex align-items-center position-relative py-2 mb-3">
+<div class="d-flex align-items-center position-relative py-3 mb-3 px-2">
 
     <!-- Tombol Back -->
-    <a href="{{ $backUrl }}" 
-        class="position-absolute start-0 ms-3" 
+    <a href="{{ $backUrl }}"
+        class="position-absolute start-0 ms-3"
         style="z-index: 10;">
         <img src="{{ asset('images/back-button.png') }}" width="24">
     </a>
 
-    <!-- Judul -->
-    <h5 class="fw-bold text-center w-100 m-0">
-        {{ $title }} <!--nama halaman yang akan ditampilkan-->
+    <!-- Judul (tanpa bold) -->
+    <h5 class="text-center w-100 m-0" style="font-weight: 500;">
+        {{ $title }}
     </h5>
 </div>
