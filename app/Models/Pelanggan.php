c0cfde6a5ac9fabf30b318e6f676650df54f717d
tsanitashafa <tsanitashafa@gmail.com>
2025-12-02 09:22:33 +0700

    revisi

--- app/Models/Pelanggan.php
+++ app/Models/Pelanggan.php
@@ -9,15 +9,8 @@ class Pelanggan extends Model
     protected $table = 'pelanggan';
     protected $primaryKey = 'pelangganid';
     public $timestamps = false;
-<<<<<<< HEAD
-<<<<<<< HEAD
     public $incrementing = true;
     protected $keyType = 'int';
-=======
->>>>>>> origin/dev-5026231088-Tsanita-Shafa-Hadinanda
-=======
->>>>>>> origin/dev-5026231105-Mirza-Fathi-Taufiqurrahman
-
     protected $fillable = [
         'nama',
         'alamat',
